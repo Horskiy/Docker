cf7c099b34ef7f24e0fe8c2b1ec896fbc9bd03f5
Horskiy <horskiy@gmail.com>
2021-09-17 22:36:21 +0000

    Working

--- nginx/test_log.sh
+++ nginx/test_log.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# service nginx start
-
 NGINX_LOG=/tmp
 
 
@@ -11,8 +9,8 @@ SIZE=$(stat -c%s $NGINX_LOG/cat_log.log) #work
 DC=$(date)
 if [[ $SIZE -lt 500000 ]]; then
 	cat /var/log/nginx/access.log /var/log/nginx/error.log > $NGINX_LOG/cat_log.log;
-        awk '/4**/' $NGINX_LOG/cat_log.log >>$NGINX_LOG/400.log;
-        awk '/5**/' $NGINX_LOG/cat_log.log >>$NGINX_LOG/500.log;
+        awk '/404/' $NGINX_LOG/cat_log.log >>$NGINX_LOG/400.log;
+        awk '/502/' $NGINX_LOG/cat_log.log >>$NGINX_LOG/500.log;
 	else
 	cat /dev/null > $NGINX_LOG/cat_log.log
 	echo "Clear file cat_log.txt $DC" >> $NGINX_LOG/clear_cat_log.log;
@@ -22,3 +20,4 @@ if [[ $SIZE -lt 500000 ]]; then
 fi
 sleep 5;
 done
+
